cfdb8845ce56ae38bb94b2465966864d500412ef
David Steinberg <david@resium.com>
2016-12-21 22:20:09 -0800

    Stream raw sequence

--- client.js
+++ client.js
@@ -1,23 +1,39 @@
 var grpc = require('grpc');
 var config = require('./config.js');
 
-var service = grpc.load({root: '.', file: 'grpc.proto'}).streaming_grpc_store;
+var service = grpc.load({root: '.', file: 'write.proto'}).sequence_store;
 
 var credentials = grpc.credentials.createInsecure();
 
-var client = new service.Store(config.grpc.host + ':' + config.grpc.port, credentials);
+var client = new service.RawSequenceStore(config.grpc.host + ':' + config.grpc.port, credentials);
+
+var started_writing = Date.now();
 
 var call = client.store(function(err, res) {
-    for (var i in res.keys) {
-        var innercall = client.retrieveone({key: res.keys[i].key}, function(err, res) {
-            console.log(err);
-            console.log(res)
-        })
-    }
+    console.log(res);
+    console.log(err);
+    var started_reading = Date.now();
+    var c = client.get({"raw_sequence_id": res.raw_sequence_id});
+    var buffer = "";
+    c.on('data', function(msg){
+        buffer += msg.bases;
+        console.log(buffer.length);
+    });
+    c.on('end', function(err, res){
+        console.log(buffer);
+        console.log(buffer.length);
+        var done_reading = Date.now();
+        var diff = done_reading - started_reading;
+        console.log("Done reading :" + diff);
+        console.log("Time per base: " + diff / buffer.length )
+    });
 });
 
 for (var i = 0; i < 100; i++) {
-    call.write({my_type: {msg: 'hi ' + i}});
+    call.write({"bases": "ATGCCTGATACATAGATACA---" + i});
 }
 
 call.end();
+var done_writing = Date.now();
+console.log(done_writing);
+console.log("Time to write: " + (done_writing - started_writing))
--- index.js
+++ index.js
@@ -3,13 +3,26 @@ var SHA3 = require('sha3');
 
 var config = require('./config');
 
-var service = grpc.load({root: '.', file: 'grpc.proto'}).streaming_grpc_store;
+var block_size = 512; // The storage implements a binary tree that blocks
+                      // sequence in increments of this maximum length.
+                      //
+                      // Choosing a biologically relevant number will
+                      // naturally create raw sequence records for biological
+                      // sequences.
+                      //
+                      // Choosing a modulus block size with relevant biological
+                      // magic numbers will further improve the quality of
+                      // the index. For example, if the sequence evenly has
+                      // a modulus of three one may desire to represent each
+                      // word individually.
+
+var service = grpc.load({root: '.', file: 'write.proto'}).sequence_store;
 var server = new grpc.Server();
 
-server.addProtoService(service.Store.service, 
+server.addProtoService(service.RawSequenceStore.service, 
 {
     store: store,
-    retrieveone: retrieveone
+    get: get
 });
 
 function getkey(serialized){
@@ -20,26 +33,60 @@ function getkey(serialized){
   return d.digest('hex');
 }
 
+function storesequence(bases) {
+    var leaves = [];
+    if (bases.length > block_size) {
+        leaves = [storesequence(bases.slice(0, bases.length / 2)), 
+        storesequence(bases.slice(bases.length / 2, bases.length))];
+    }
+    var d = new SHA3.SHA3Hash();
+    d.update(bases);
+    if (leaves.length > 0) {
+        storage[d.digest('hex')] = {raw_sequence_ids: leaves};
+    } else {
+        storage[d.digest('hex')] = {bases: bases};
+    }
+    console.log(storage);
+    return d.digest('hex');
+}
+
+function getsequence(raw_sequence_id) {
+    var root = storage[raw_sequence_id];
+    var buffer = "";
+    console.log(root);
+    if (root.raw_sequence_ids) {
+        console.log("Reading leaves");
+        root.raw_sequence_ids.forEach(function(rsid) {
+            buffer += getsequence(rsid);
+        });
+    } else {
+        buffer += root.bases;
+    }
+    // console.log('get sequence buffer')
+    // console.log(buffer);
+    return buffer;
+}
+
 var storage = {};
 
 function store(call, callback) {
-  var keys = [];
+  var buffer = "";
+  var d = new SHA3.SHA3Hash();
   call.on('data', function(msg) {
-    var encoded = (new service.StoreMessage(msg)).encode64()
-    var key = getkey(encoded);
-    storage[key] = encoded;
-    keys.push({key: key});
+    var message = new service.RawSequenceStreamWriteRequest(msg);
+    buffer += msg.bases;
+    //d.update(msg.bases);
   });
   call.on('end', function() {
-    callback(null, {
-        keys: keys
-    });
+    var resp = {"raw_sequence_id": storesequence(buffer)};
+    console.log(resp);
+    callback(null, new service.RawSequenceStreamWriteResponse(resp));
   });
 }
 
-function retrieveone(call, callback) {
-    var k = service.StoreMessage.decode64(storage[call.request.key]);
-    callback(null, k);
+function get(call) {
+    call.write({"bases": getsequence(call.request.raw_sequence_id)})
+    call.end();
 }
 
 server.bind(config.grpc.host + ':' + config.grpc.port, grpc.ServerCredentials.createInsecure());
